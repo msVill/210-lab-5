e0113394d6fd794ae4d468387130122f8de75546
msVill <msv.ltmc@gmail.com>
2026-02-11 23:33:32 -0800

    Changed a few things and want to step back to reasses to understand.

--- 210-lab-5-starter.cpp
+++ 210-lab-5-starter.cpp
@@ -1,10 +1,11 @@
 #include <iostream>
 using namespace std;
 
-int swap(int &, int &);
+int swap(int &, int &); // function prototype.
 
 int main() {
-    int x = 5, y = 10;
+    int x = 5, 
+        y = 10;
     cout << "x = " << x << "  | y = " << y << endl;
 
     cout << "Swapping...\n";
@@ -14,7 +15,7 @@ int main() {
     return 0;
 }
 
-int swap(int &a, int &b) {
+int swap(int *a, int *b) { // we want to swap the variables using pointers.
     int temp = a;
     a = b;
     b = temp;
