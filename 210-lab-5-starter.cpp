0f971f5150bc07335ab2dacd995bfeeefa4e543a
msVill <msv.ltmc@gmail.com>
2026-02-12 12:21:41 -0800

    Added indirection operator to function swap

--- 210-lab-5-starter.cpp
+++ 210-lab-5-starter.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 using namespace std;
 
-int swap(int &, int &); // function prototype.
+int swap(int *, int *); // function prototype.
 
 int main() {
     int x = 5, 
@@ -9,15 +9,15 @@ int main() {
     cout << "x = " << x << "  | y = " << y << endl;
 
     cout << "Swapping...\n";
-    int sum = swap(x, y);
+    int sum = swap(*x, *y);
     cout << "x = " << x << " | y = " << y << endl;
     cout << "sum = " << sum << endl;
     return 0;
 }
 
-int swap(int &a, int &b) { // we want to swap the variables using pointers.
-    int temp = a;
-    a = b;
-    b = temp;
-    return a + b;
+int swap(int *a, int *b) { // we want to swap the variables using pointers.
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+    return *a + *b;
 }
